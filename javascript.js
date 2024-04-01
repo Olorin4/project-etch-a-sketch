f6f1c036aba2aa17d6ba39419fef0ae49755a817
Nick Kalas <nick.kalas@proton.me>
2024-04-01 17:07:04 +0300

    Add function for creating a 16x16 grid of divs

--- javascript.js
+++ javascript.js
@@ -1,2 +1,6 @@
 // Function for creating a 16x16 grid of square divs
 let grid = document.querySelector(".container");
+for (let i = 0; i <= 16; i++) {
+    let div = document.createElement("div");
+    grid.appendChild(div);
+}
